1ea5e4ab1b45a1945df54b2b345c192279515d4f
isman arale <ismailarales@gmail.com>
2023-09-16 03:07:10 -0500

    testing val api req

--- valorant-sandbox/src/App.jsx
+++ valorant-sandbox/src/App.jsx
@@ -6,22 +6,27 @@ import { useState } from "react"
 
 
 export default function App() {
+
   // state
-  const [] = useState([])
+  const [valData, setValData] = useState([])
 
   // useEffects
   useEffect(() => {
-
+    async function getValorantData() {
+      try {
+        const res = await fetch(`https://valorant-api.com/v1/agents`);
+        if (!res.ok) throw new Error("OMG BRO THE FETCH REQUEST IS NOT 200 BRO WTF ITS NOT WORKING BRO WHAT HAPPENED BRO?!??! B RO FIX THIS ASAP NOW!")
+        const data = res.json();
+        setValData(data)
+      } catch (err) {
+        console.error(err.MESSAGE)
+      }
+    }
   }, [])
 
   // funcs
-  function getValorantData() {
-    try {
 
-    } catch (err) {
-      console.log(err)
-    }
-  }
+
   return (
     <>
       <header>
