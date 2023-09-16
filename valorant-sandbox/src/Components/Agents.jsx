dd529d827f34addb4df39ad6b632aee59c6748ef
isman arale <ismailarales@gmail.com>
2023-09-16 06:57:18 -0500

    added border radius to card container

--- valorant-sandbox/src/Components/Agents.jsx
+++ valorant-sandbox/src/Components/Agents.jsx
@@ -3,7 +3,7 @@ export default function Agent({ agentData, isDark }) {
     // console.log(agentData.displayName)
 
     return <>
-        {agentData && agentData.map(agent => <div key={agent.uuid} className={`border-[2px] p-2 max-w-sm  flex flex-col ${!isDark ? "border-lm-primary" : "border-dm-primary"}`}>
+        {agentData && agentData.map(agent => <div key={agent.uuid} className={`border-[2px] p-2 max-w-sm rounded-md flex flex-col ${!isDark ? "border-lm-primary" : "border-dm-primary"}`}>
             <img className="max-w-[72px] md:max-w-[90px] lg:max-w-[120px]" src={agent.displayIcon} alt="" />
             <h1 className="text-lg font-bold underline  inline-block mr-3">{agent.displayName}</h1>
             <div className="flex items-center  mb-3">
