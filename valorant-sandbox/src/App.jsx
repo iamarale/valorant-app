edb98c7a1b445f074a60c440bfc16b0a7f275fa0
isman arale <ismailarales@gmail.com>
2023-09-16 03:03:28 -0500

    starter files

---
+++ valorant-sandbox/src/App.jsx
@@ -0,0 +1,32 @@
+// https://valorant-api.com/v1/agents
+
+import { useEffect } from "react"
+import { useState } from "react"
+
+
+
+export default function App() {
+  // state
+  const [] = useState([])
+
+  // useEffects
+  useEffect(() => {
+
+  }, [])
+
+  // funcs
+  function getValorantData() {
+    try {
+
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  return (
+    <>
+      <header>
+
+      </header>
+    </>
+  )
+}
