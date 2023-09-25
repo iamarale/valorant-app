6ac4665e95b80c16c7126657eefaa260aabec6d2
iamarale <ismailarales@gmail.com>
2023-09-25 12:38:44 -0500

    added dynamic loader state

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Navigation } from "./Components/Navigation";
 export default function App() {
   // state
   const [query, setQuery] = useState('agents');
+  const [isLoading, setIsLoading] = useState(false)
   const [valData, setValData] = useState([]);
   const [isDark, setIsDark] = useState(false)
 
@@ -15,10 +16,11 @@ export default function App() {
   useEffect(() => {
     async function getValorantData() {
       try {
+        setIsLoading(true)
         const res = await fetch(`https://valorant-api.com/v1/${query}`);
         if (!res.ok) throw new Error("There was an error fetching the dagrGRGARGARGta");
         const data = await res.json();
-
+        setIsLoading(false)
         // valData holds api data
         setValData(data.data)
       } catch (err) {
@@ -29,8 +31,6 @@ export default function App() {
     getValorantData()
   }, [query]);
 
-  console.log(valData[0])
-  console.log(valData[0]?.isPlayableCharacter)
 
   return (
     <div className={`max-h-max px-2 duration-200 ${!isDark ? "bg-lm-background text-lm-text " : "bg-dm-background text-dm-text"}`}>
@@ -38,7 +38,7 @@ export default function App() {
       <Navigation />
       <main className="grid gap-4 justify-center  container mx-auto sm:grid-cols-2 sm:justify-items-center lg:grid-cols-3">
         {/* Agents */}
-        <Agents isDark={isDark} agentData={valData} />
+        {isLoading ? <h1 className="text-center text-4xl">Loading</h1> : <Agents isDark={isDark} agentData={valData} />}
       </main>
     </div>
   )
