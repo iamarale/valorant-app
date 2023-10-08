79cf782ae023c40991bae2b7612ed122a2285421
iamarale <ismailarales@gmail.com>
2023-10-07 23:16:34 -0500

    save point

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,9 +2,9 @@ import { Navigation } from "./Navigation";
 
 export default function Header() {
 
-    return <header className="bg-background">
-        <h1 className="text-3xl font-bold">The Valorant Info Hub</h1>
-        <h2 className="text-xl inline-block">All things valorant</h2>
+    return <header className="bg-background p-2 container mx-auto">
+        <h1 className="text-3xl font-bold">The Valorant Spot</h1>
+        <h2 className="text-xl inline-block">Learn about agents, see all skins, and view all playable weapons </h2>
         <Navigation />
     </header>
 }
--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { NavLink } from 'react-router-dom';
 
 export const Navigation = () => {
-    const navStyles = `text-primary-light font-bold text-2xl text-primary opacity-50`;
+    const navStyles = `text-primary-light font-bold text-2xl`;
 
     return (
         <nav>
-            <ul className='flex justify-center gap-10'>
+            <ul className='flex justify-center gap-10 py-6'>
                 <li><NavLink to="/" className={navStyles}>Agents</NavLink></li>
                 <li><NavLink to="/bundles" className={navStyles}>Bundles</NavLink></li>
                 <li><NavLink to="/kits" className={navStyles}>Kits</NavLink></li>
