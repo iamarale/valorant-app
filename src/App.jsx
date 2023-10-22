1028c65b26a5488b8f07759f0a0e650e5b6e8bc6
iamarale <ismailarales@gmail.com>
2023-10-22 00:33:09 -0500

    Fetched bundles and kits, but this method only temp

--- src/App.jsx
+++ src/App.jsx
@@ -6,52 +6,68 @@ import Bundles from "./Pages/Bundles";
 import Homepage from "./Pages/Homepage";
 import Error from "./Pages/Error";
 
-export default function App() {
-  // state
-  const [isLoading, setIsLoading] = useState(false)
-  const [agents, setAgents] = useState([])
-  const [selectedId, setSelectedId] = useState('')
 
-  // useEffects
+export default function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedId, setSelectedId] = useState('');
+  const [agents, setAgents] = useState([]);
+  const [bundles, setBundles] = useState([]);
+  const [weapons, setWeapons] = useState([]); // State variable for weapons
+  const [gear, setGear] = useState([]); // State variable for gear
+
   useEffect(() => {
     async function getAgentData() {
       try {
-        // set
-        setIsLoading(true)
-        const res = await fetch(`https://valorant-api.com/v1/agents`);
-        const data = await res.json();
-        setIsLoading(false)
+        setIsLoading(true);
 
-        // puts all data into agents
-        setAgents(data.data)
+        // Fetch Agents 
+        const agentResponse = await fetch(`https://valorant-api.com/v1/agents`);
+        const agentData = await agentResponse.json();
+
+        // Fetch bundles 
+        const bundlesResponse = await fetch(`https://valorant-api.com/v1/bundles`);
+        const bundlesData = await bundlesResponse.json();
+
+        // Fetch weapons 
+        const weaponsResponse = await fetch(`https://valorant-api.com/v1/weapons`);
+        const weaponsData = await weaponsResponse.json();
+
+        // Fetch gear 
+        const gearResponse = await fetch(`https://valorant-api.com/v1/gear`);
+        const gearData = await gearResponse.json();
+
+        // if any of the responses 
+        if (agentResponse.status !== 200 || bundlesResponse.status !== 200 || weaponsResponse.status !== 200 || gearResponse.status !== 200) {
+          throw new Error("Error fetching the data");
+        }
+
+
+        setIsLoading(false);
+
+        // stores the data from the responses in state
+        setAgents(agentData.data);
+        setBundles(bundlesData.data);
+        setWeapons(weaponsData.data);
+        setGear(gearData.data);
       } catch (err) {
-        console.error(err)
+        console.error(err);
       }
     }
 
-    getAgentData()
+    getAgentData();
   }, [selectedId]);
 
+  // ... Rest of your code
 
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage isLoading={isLoading} agents={agents} setSelectedId={setSelectedId} selectedId={selectedId} />}>Agent</Route >
-        <Route path="bundles" element={<Bundles />}>Bundles</Route>
+        <Route path="bundles" element={<Bundles bundles={bundles} />}>Bundles</Route>
         <Route path="kits" element={<Kits />}>Kits</Route>
         <Route path="*" element={<Error />}>Kits</Route>
       </Routes>
     </BrowserRouter>
   )
 }
-
-
-
-// <div className={`min-h-full px-2 duration-200 bg-background`}>
-//   <Header />
-//   <Navigation />
-
-//   {/* displays content based on nav option*/}
-//   <MainContent isLoading={isLoading} setSelectedId={setSelectedId} selectedId={selectedId} agents={agents} />
-// </div>
--- src/Pages/Bundles.jsx
+++ src/Pages/Bundles.jsx
@@ -1,9 +1,12 @@
 import Header from "../Components/Header"
-export default function Bundles() {
+export default function Bundles({ bundles }) {
+    console.log(bundles)
     return (<>
         <Header />
         <main className="container mx-auto">
             <h1>Bundles</h1>
+            <input />
+            {bundles && bundles.map(bundle => <div>{bundle.displayName}</div>)}
         </main>
     </>
     )
