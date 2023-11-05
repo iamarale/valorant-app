73f5980e21199e53a28991be0bf14e1d6cef10a8
iamarale <ismailarales@gmail.com>
2023-11-05 15:08:11 -0600

    made button reusable

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,4 +1,4 @@
 export default function Button({ onClick, children }) {
-    return <button onClick={onClick}>{children}</button>
+    return <button className="border-2 border-dark-gray px-2 rounded duration-200 hover:bg-valorant-red hover:text-light-gray" onClick={onClick}>{children}</button>
 }
 
