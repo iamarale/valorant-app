38ddf75eed71aec21826c6c0067166a88a921b03
iamarale <ismailarales@gmail.com>
2023-11-06 06:04:34 -0600

    input functionality working

--- src/Pages/Bundles.jsx
+++ src/Pages/Bundles.jsx
@@ -3,29 +3,48 @@ import Header from "../components/Header";
 import Button from "../components/UI/Button";
 
 export default function Bundles({ bundles }) {
-    const [favoriteBundles, setFavoriteBundle] = useState([])
+    const [favoriteBundles, setFavoriteBundle] = useState([]);
+    const [searchBundle, setSearchBundle] = useState("")
 
-    function addtoFavorite(fav) {
-        setFavoriteBundle([...favoriteBundles, fav])
-        console.log(favoriteBundles)
+    function addtoFavorite(favorite) {
+        setFavoriteBundle([...favoriteBundles, favorite])
     }
+
+    const filteredBundles = bundles.filter(bundle => bundle.displayName.toLowerCase().includes(searchBundle.toLowerCase()));
+
 
     return <div className="h-full">
         <Header />
-        <main className="container mx-auto flex items-center flex-col text-dark-gray mt-6">
-            <form className="max-w-xl flex mx-auto container">
-                <input className="border-2 rounded-l- border-r-0 flex-[1] border-dark-gray px-2 py-2 lg:py-1" type="text" placeholder="Search for a bundle" />
-                <Button leftNotRound={true}>Search </Button>
-
+        {searchBundle}
+        <main className="container mx-auto flex items-center flex-col text-dark-gray mt-6 p-2">
+            <form className="md:max-w-2xl flex mx-auto container" onSubmit={(e) => e.preventDefault()}>
+                <input
+                    className="border-2 rounded-lg  flex-[1] border-dark-gray px-2 py-2 lg:py-1"
+                    type="text"
+                    placeholder="Search for a bundle"
+                    onChange={(e) => setSearchBundle(e.target.value)}
+                    value={searchBundle}
+                />
             </form>
-            {bundles && bundles.map(bundle => <div key={bundle.uuid} className="border-2 border-dark-gray my-2 rounded-lg">
-                <img className="max-w-xl rounded-t-lg" src={bundle.displayIcon} alt={`A picture of ${bundle.displayName}`} />
+            {!searchBundle ? bundles.map(bundle => <div key={bundle.uuid} className="border-2 border-dark-gray my-2 rounded-lg">
+                <img className="md:max-w-2xl rounded-t-lg" src={bundle.displayIcon} alt={`A picture of ${bundle.displayName}`} />
                 <div className="p-2">
-                    <h1 className="text-lg font-thin pb-2">{bundle.displayName}</h1>
+                    <h1 className="text-lg underline font-thin pb-2">{bundle.displayName}</h1>
                     <Button onClick={() => addtoFavorite(bundle)}>Favorite</Button>
                 </div>
-            </div>)}
+            </div>) : (
+                filteredBundles.map(bundle => (
+                    <div key={bundle.uuid} className="border-2 border-dark-gray my-2 rounded-lg">
+                        <img className="md:max-w-2xl rounded-t-lg" src={bundle.displayIcon} alt={`A picture of ${bundle.displayName}`} />
+                        <div className="p-2">
+                            <h1 className="text-lg underline font-thin pb-2">{bundle.displayName}</h1>
+                            <Button onClick={() => addtoFavorite(bundle)}>Favorite</Button>
+                        </div>
+                    </div>
+                ))
+            )}
         </main>
     </div>
 }
 
+// <div key={bundle.uuid} className="border-2 border-dark-gray my-2 rounded-lg"> </div>
