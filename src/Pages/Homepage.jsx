4eac00f16c604d3d18f7742829b7b383dff6f253
iamarale <ismailarales@gmail.com>
2023-11-06 01:42:48 -0600

    added copy to homepage

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,9 +1,17 @@
 import Header from "../components/Header";
 
 export default function Homepage() {
-    return <div className="h-full text-valorant-red">
+    return <div className="h-ful text-dark-gray">
         <Header />
-        <h1 className="">Welcome to the valorant info</h1>
+        <main className="pt-4 container mx-auto ">
+            <h1 className="text-3xl font-light">Find all things valorant! We provide you with quick and easy access to the latest agents, bundles and weapons released to <span className="text-valorant-red">Riot games Valorant</span>!</h1>
+
+            <ul className="mt-8">
+                <li className="list-disc text-sm"><span className="text-md font-semibold text-valorant-red uppercase">Simplicity:</span> User friendly UI to help you navigate and search for what you need without hassle or frustration</li>
+                <li className="list-disc text-sm"><span className="text-md font-semibold text-valorant-red uppercase">Up-to-Date:</span> Newly released agents, bundles and weapons will be released here so youll be first to know!</li>
+                <li className="list-disc text-sm"><span className="text-md font-semibold text-valorant-red uppercase">Easy to understand:</span> The information offered will help you understand and succeed in valorant</li>
+            </ul>
+        </main>
     </div>
 }
 
